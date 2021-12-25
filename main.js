7cc8506a128cd67045068f9d8b6104aa92aa82dc
duong <tovanduonghp1996@gmail.com>
2021-12-25 14:20:39 +0700

    add

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 300) {
-          btn.addClass('show');
-        } else {
-          btn.removeClass('show');
-        }
-    })
-})
